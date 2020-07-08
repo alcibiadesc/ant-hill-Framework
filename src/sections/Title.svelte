d05964cdd9be43cb72590afc9d662335744b3abb
Al6 <acabdia6@users.noreply.github.com>
2020-07-08 11:06:40 +0200

    first commit

---
+++ src/sections/Title.svelte
@@ -0,0 +1,16 @@
+<style>
+  .lookhere {
+    color: #ff7d87;
+  }
+</style>
+
+<div class="pa1 ml5 w-60">
+  <h1 class=" courier f1-ns f-subheadline-l lh-title-l mb0">Ant Hill</h1>
+  <h2 class="fw3 f3-ns f2-l sans-serif mt2">
+    Un framework para generar
+    <span class="lookhere">feedback loops</span>
+    orientados a la aportación continua de
+    <span class="lookhere">valor</span>
+
+  </h2>
+</div>
