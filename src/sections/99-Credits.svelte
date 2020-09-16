b0ca0371e05254d77f560019bfbab58988e5d793
Al6 <acabdia6@users.noreply.github.com>
2020-09-16 11:40:28 +0100

    tranlate to spanish

--- src/sections/99-Credits.svelte
+++ src/sections/99-Credits.svelte
@@ -1,7 +1,7 @@
 <hr class="mt5 " />
 
 <p class="mt4 mh4 pb0 mb1 tc courier fw1">
-  made with ❤️ by
+  hecho con ❤️ por
   <a
     href="https://www.linkedin.com/in/alcibiadesc/"
     target="#blank"
