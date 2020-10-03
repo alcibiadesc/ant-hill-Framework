338352585b82d11fa5c930f6ef1b394d5be8e08b
Alcibíades Cabral Díaz <acabdia@gmail.com>
2020-10-03 11:13:13 +0200

    stage

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,12 +1,7 @@
 <script>
-	import { showStore } from './../store/sidebar-store.js';
-
+  import { showStore } from "./../store/sidebar-store.js";
 
   import { fly } from "svelte/transition";
-
-
-  
-
 </script>
 
 <style>
@@ -23,44 +18,49 @@
     z-index: 2;
   }
 
-  svg:hover{
+  svg:hover {
     fill: #ff7d87;
   }
 
-  h3:hover{
- color: #ff7d87;
-  }
-
-  h3:focus{
+  h3:hover {
     color: #ff7d87;
   }
 
-  h3{
-    color: white;
-    
+  h3:focus {
+    color: #ff7d87;
   }
-  a{
+
+  h3 {
+    color: white;
+  }
+  a {
     text-decoration: none;
   }
 </style>
 
-
-  <nav transition:fly={{ x: -250, opacity: 1 }} class="bl black" on:clickoutside={() => {$showStore = false}}>
-
-<div on:click={()=> $showStore = !$showStore} >
-<svg class="grow ml4 mt3 absolute top-0 left-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="28px" height="28px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
-</div>
-    <div class="ml3 mt3">
-
-
-        <a href="." ><h3 class="link underline-hover grow courier">Inicio</h3></a>
-        <a href="guia" ><h3 class="link underline-hover grow courier">Guia</h3></a>
-        <a href="blog"on:click={()=> $showStore = !$showStore}><h3 class="link underline-hover grow courier">Blog</h3></a>
-      
-    </div>
-
-    
-  </nav>
-
-
-
+<nav
+  transition:fly={{ x: -250, opacity: 1 }}
+  class="bl black"
+  on:clickoutside={() => {
+    $showStore = false;
+  }}>
+  <div on:click={() => ($showStore = !$showStore)}>
+    <svg
+      class="grow ml4 mt3 absolute top-0 left-0"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="white"
+      width="28px"
+      height="28px"><path d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" /></svg>
+  </div>
+  <div class="ml3 mt3">
+    <a href="."><h3 class="link underline-hover grow courier">Inicio</h3></a>
+    <a href="guia"><h3 class="link underline-hover grow courier">Guia</h3></a>
+    <a href="blog" on:click={() => ($showStore = !$showStore)}><h3
+        class="link underline-hover grow courier">
+        Blog
+      </h3></a>
+  </div>
+</nav>
