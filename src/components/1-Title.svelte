3b53131a7256a64062143b879a830aef621c5de6
Alcibíades Cabral Díaz <alcibiades@MacBook-Pro-de-Alcibiades.local>
2020-10-08 13:29:41 +0200

    change title size

--- src/components/1-Title.svelte
+++ src/components/1-Title.svelte
@@ -5,12 +5,11 @@
 </style>
 
 <div class="ph1 pb1 w-80 " id="title">
-
   <a href=".">
     <h1 class=" courier f1-ns f-subheadline-l lh-title-l mb0">Ant Hill</h1>
   </a>
 
-  <p class="fw3 f3-ns f2-l mt2 ">
+  <p class="fw3 f3-ns f2-l f2-m f3 mt2 ">
     Un framework para generar
     <span class="lookhere">feedback loops</span>
     orientados a la aportación continua de
